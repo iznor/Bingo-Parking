74017295b90b102bf058f3a01488611ce5382240
iZnoR <76132625+iznor@users.noreply.github.com>
2021-12-11 18:13:19 +0200

    docs updated, including export

--- models/parkings.js
+++ models/parkings.js
@@ -1,10 +1,23 @@
+const { ObjectId, Double } = require('bson');
 const { Schema, model, Mongoose } = require('mongoose');
+const { personSchema } = require('./persons');
+
+const locationSchema = new Location({
+    lng: { Type: Double },
+    lat: { Type: Double }
+});
 
 const parkingSchema = new Schema({
-    /*SCHEMA*/
+    parkingID: { Type: Number },
+    person: personSchema,
+    location: locationSchema,
+    dateStart: { Type: Date },
+    dateEnd: { Type: Date },
+    price: { Type: Double },
+    active: { Type: Boolean }
 }, { collection: 'parkings' });
-/*
+
 const Parking = model('Parking', parkingSchema);
 
 module.exports = Parking; 
-*/
+
--- models/persons.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-const personSchema = new Schema({
-    /*SCHEMA*/
-}, { collection: 'persons' });
-/*
-const Person=model('Person', personSchema);
-
-module.exports = {Person,personSchema};
-*/
