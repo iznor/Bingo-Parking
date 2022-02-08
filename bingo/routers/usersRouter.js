dc16bf74fb4190295d61638c83e776980477040f
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-08 02:58:13 +0200

    usersRouter updated

--- bingo/routers/usersRouter.js
+++ bingo/routers/usersRouter.js
@@ -4,9 +4,5 @@ const { usersController } = require('../controllers/usersController');
 const usersRouter = new Router();
 
 usersRouter.post('/login', usersController.login);
-// usersRouter.get('/:id', usersController.getParkingById);
-// usersRouter.post('/', usersController.addParking);
-// usersRouter.put('/:id', parkingsController.editParking);
-// usersRouter.delete('/:id', parkingsController.deleteParking);
 
 module.exports = { usersRouter };
