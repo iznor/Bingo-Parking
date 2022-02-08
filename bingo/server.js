8d5dbf38579ee388bb67a61711dc3fe7cd8f6d14
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-08 02:35:52 +0200

    server.js updated

--- bingo/server.js
+++ bingo/server.js
@@ -2,19 +2,24 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
+
 app.use(express.json());
+app.use(cors());
 const { parkingsRouter } = require("./routers/parkingsRouter");
 const { personsRouter } = require("./routers/personsRouter");
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header('Access-Control-Allow-Headers', "*");
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.set('Content-Type', 'application/json; charset=utf-8');
-  next();
-});
+const { usersRouter } = require("./routers/usersRouter");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   res.header('Access-Control-Allow-Headers', "*");
+//   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.set('Content-Type', 'application/json; charset=utf-8');
+//   next();
+// });
 app.use('/api/parkings', parkingsRouter);
 app.use('/api/persons', personsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(express.urlencoded({ extended: true }));
 router.use((req,res,next) =>{
