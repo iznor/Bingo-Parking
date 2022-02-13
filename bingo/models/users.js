24d6c68498e26236e2c8ba1c5c39ad14e18811ca
Ayham10 <ayham.huss@gmail.com>
2022-02-13 12:17:56 +0200

    test1

--- bingo/models/users.js
+++ bingo/models/users.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema({
     email: String,
     password: String
+    // orders : [parkingId]
 }, { collection: 'users' }) ;
 
 const Users=model('Users', userSchema);
