d42e93ae8b08ba2f4ecb07b7845006f054b52d7b
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-14 00:48:48 +0200

    server and constants updated with authentications

--- bingo/server.js
+++ bingo/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const userAuth = require("./middleware/userAuth");
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-const { parkingsRouter } = require("./routers/parkingsRouter");
-const { personsRouter } = require("./routers/personsRouter");
+const { parkingsRouter }  = require("./routers/parkingsRouter");
+const { personsRouter }   = require("./routers/personsRouter");
+const { userRouter }      = require("./routers/usersRouter");
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
@@ -13,8 +16,11 @@ app.use((req, res, next) => {
   res.set('Content-Type', 'application/json; charset=utf-8');
   next();
 });
+
+
 app.use('/api/parkings', parkingsRouter);
 app.use('/api/persons', personsRouter);
+app.use('/api/user',  userRouter);
 
 app.use(express.urlencoded({ extended: true }));
 router.use((req,res,next) =>{
