d015ace811b16310dda15fb741083c90f756edaf
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-14 00:46:01 +0200

    user controller updated with JWT login logics

--- bingo/controllers/usersController.js
+++ bingo/controllers/usersController.js
@@ -1,11 +1,24 @@
 const { Users } = require('../models/users');
 const logger = require('../logger');
+const userAuth = require('../middleware/userAuth')
+
 
 exports.usersController = {
     async login(req, res) {
        const user = await Users.findOne({email :req.body.email});
-       if(user){
-           return res.status(200).json({...user.toObject()});
+       if(!user){
+           return res.status(404).json({error: 'User Not Found'})
+       }
+       const passwordMatch = req.body.password === user.password
+       if(passwordMatch){
+           const token = await userAuth.getToken(user.email)
+           return res.status(200).json({
+               ...user.toObject(), 
+               token: token
+            });
+       }
+       else{
+           return res.status(401).json({error: 'Authentication Failed'});
        }
     }
 };
