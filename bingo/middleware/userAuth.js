a80982c4d1f63fc28b7dbeb8139cec397ee64a37
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-14 23:12:03 +0200

    small fixes

--- bingo/middleware/userAuth.js
+++ bingo/middleware/userAuth.js
@@ -1,37 +1,41 @@
 const jwt = require('jsonwebtoken')
-const consts = require('../constants' );
+const consts = require('../constants');
 const { SECRET_KEY } = consts;
 
 module.exports = {
-    getToken: (user)=>{
-        return new Promise((resolve, reject)=>{
-            try{
-                const token = `${jwt.sign(user,SECRET_KEY)}`
+    getToken: (user) => {
+        return new Promise((resolve, reject) => {
+            try {
+                const token = `${jwt.sign(user, SECRET_KEY)}`
                 resolve({
                     token: token
                 })
             }
-            catch(err){
+            catch (err) {
                 reject(err)
             }
         })
     },
     verifyToken: async (req, res, next) => {
         //Missing Token
-        if(!req.headers.authorization){
-            res.status(401).send({error: 'Authorization headers not present'})
+        if (!req.headers.authorization) {
+            res.status(401).send({ error: 'Authorization headers not present' })
         }
-        try{
-            const token = req.headers.authorization.split(' ')[1]
-            const resp = jwt.verify(token,SECRET_KEY)
-            if(!resp){
-                res.status(401).send({error: 'Unauthorized Access'})
+        else {
+            try {
+                const token = req.headers.authorization.split(' ')[1]
+                const resp = jwt.verify(token, SECRET_KEY)
+                if (!resp) {
+                    res.status(401).send({ error: 'Unauthorized Access' })
+                }
+                else {
+                    next()
+                }
             }
-            next()
+            catch (err) {
+                res.status(401).send({ error: 'Unauthorized Access' })
+            }
+
         }
-        catch(err){
-            res.status(401).send({error: 'Unauthorized Access'})
-        }
-        
     }
 }
