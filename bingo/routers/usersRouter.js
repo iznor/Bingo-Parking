da43f0e7863d4e82b8983dcfac28c3b3d132892a
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-15 12:45:48 +0200

    added auth logics to new user routers

--- bingo/routers/usersRouter.js
+++ bingo/routers/usersRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const { usersController } = require('../controllers/usersController');
+const userAuth = require('../middleware/userAuth')
 
 const userRouter = new Router();
 
 userRouter.post('/login', usersController.login);
-userRouter.put('/:email', usersController.editUser);
-userRouter.get('/:email/orders', usersController.getUserOrdersByEmail);
+userRouter.put('/:email',userAuth.verifyToken, usersController.editUser);
+userRouter.get('/:email/orders',userAuth.verifyToken , usersController.getUserOrdersByEmail);
 module.exports = { userRouter };
